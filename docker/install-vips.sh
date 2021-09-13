9dd7cc3692de74b975c2f70da6a1fb4c1262ddc9
Daniel Jacobs <danielhunterjacobs@gmail.com>
2021-09-13 12:18:06 -0400

    Try using custom docker image

---
+++ docker/install-vips.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# Copy of https://github.com/libvips/php-vips/blob/master/install-vips.sh
+
+version="8.11.3"
+HOME="/home/circleci/"
+vips_tarball=https://github.com/libvips/libvips/releases/download/v$version/vips-$version.tar.gz
+
+set -e
+
+rm -rf $HOME/vips
+curl -Ls $vips_tarball | tar xz
+cd vips-$version
+./configure --prefix=$HOME/vips "$@"
+make install
